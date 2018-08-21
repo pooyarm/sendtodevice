44b44e264a7f319769108b5098641c0db1f92e05
Pooya Rostam <pooyarm@gmail.com>
2018-08-21 15:47:54 +0430

    Remove unneeded files and fix service worker webpack command

---
+++ webpack.config.sw.js
@@ -0,0 +1,25 @@
+'use strict';
+
+const path = require('path');
+
+module.exports = (env, args) => {
+    process.env.NODE_ENV = args.mode;
+    
+    return {
+        mode: 'development',
+        entry: './src/sw/notification.js',
+        output: {
+            path: path.resolve(__dirname, "../public"),
+            filename: 'firebase-messaging-sw.js',
+        },
+        resolve: {
+            modules: [
+                "node_modules",
+                path.resolve(__dirname)
+            ],
+            extensions: [".js", ".json"],
+        },
+        context: __dirname,
+        watch: true
+    };
+}
